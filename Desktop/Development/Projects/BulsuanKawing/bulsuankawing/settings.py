5d80f1d5d83ad6ca30f86d1f4cfbdf81f7efac42
Rey <reynaldosalamatjr9.11@gmail.com>
2022-02-04 13:38:04 +0800

    settings remove imports

--- Desktop/Development/Projects/BulsuanKawing/bulsuankawing/settings.py
+++ Desktop/Development/Projects/BulsuanKawing/bulsuankawing/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
-import django_heroku
-import dj_database_url
-from decouple import config
 
 from pathlib import Path
 
@@ -190,4 +187,3 @@ EMAIL_HOST_PASSWORD ="bulsuan1904kawing2021"
 
 
 
-django_heroku.settings(locals())
