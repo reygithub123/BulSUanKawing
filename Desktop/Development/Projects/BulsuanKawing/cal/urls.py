2edfe744f6cee68d8885aac42325a60e3764d9d5
Rey <reynaldosalamatjr9.11@gmail.com>
2022-02-05 15:53:06 +0800

    updated import

--- Desktop/Development/Projects/BulsuanKawing/cal/urls.py
+++ Desktop/Development/Projects/BulsuanKawing/cal/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+
 from django.urls import path
 from . import views
 
 app_name = 'cal'
 urlpatterns = [
     path('', views.CalendarView.as_view(), name="calendar"),
-    url('event/new/', views.create_event, name='event_new'),
-    url('event/edit/(?P<event_id>\d+)/', views.view_event, name='event_edit'),
+    path('event/new/', views.create_event, name='event_new'),
+    path('event/edit/(?P<event_id>\d+)/', views.view_event, name='event_edit'),
 ]
